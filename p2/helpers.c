7cbec9cb9b3737a8615140f0139579fd160f0daf
Caaraya <christina-araya@hotmail.com>
2017-03-12 16:56:32 -0700

    send max of 4

--- p2/helpers.c
+++ p2/helpers.c
@@ -216,11 +216,8 @@ void process_packets(packet* pack, packet** window_arr, FILE* file, int* window_
 }
 //for client
 packet** bulksendDAT(int sock, struct sockaddr_in* self_address, struct sockaddr_in* partner_sa, socklen_t partner_sa_len, FILE* file,  int* current_seqno, enum system_states *stat, packet* last_received, int* last_indx){
-    //remove non acknowledged packets still in window
-    int send_packets = last_received->win;
-
-    //send up to max amount of packets
-    send_packets = MAX_WINDOW_IN_PACKETS - send_packets;
+    //get remaining window availability
+    int send_packets = (last_received->win < 5? last_received->win: 4);
 
     //save packets in array
     //packet* window_arr[send_packets + 1] = calloc((send_packets+1), sizeof(struct packet));
